8439281edf4f564e35450eb574e7c54ca8a0a55d
Ignat Sychev <amanewave@gmail.com>
2024-10-16 12:05:07 +1000

    feat: search history

--- client/src/components/search/Dropdown.jsx
+++ client/src/components/search/Dropdown.jsx
@@ -1,46 +1,86 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { memo, useState } from "react";
+import styled from "styled-components";
+import CloseIcon from "../../assets/close.svg";
 
-export const Dropdown = ({searhHistory}) => {
+export const HistoryItem = ({ elem, i, arr, setSearchHistory }) => {
+  const [historyElemActive, setHistoryElemActive] = useState(false);
 
-	
-    return (
-			<Wrapper>
-				<DropdownContent>
-					<Item>
-					
-					</Item>
-				</DropdownContent>
-			</Wrapper>
-		)
+  const removeFromHistory = () => {
+    const a = arr.filter((_, ind) => ind !== i);
+    setSearchHistory(a);
+  };
+
+  return (
+    <Item
+      onMouseEnter={() => setHistoryElemActive(true)}
+      onMouseLeave={() => setHistoryElemActive(false)}
+    >
+      <ItemWrapper>
+        {elem}
+        {historyElemActive && (
+          <StyledClose src={CloseIcon} onClick={removeFromHistory} />
+        )}
+      </ItemWrapper>
+    </Item>
+  );
 };
 
+export const Dropdown = memo(({ searchHistory, setSearchHistory }) => {
+  const SearchHistoryElem = searchHistory.map((elem, i) => (
+    <HistoryItem
+      key={Math.random()}
+      elem={elem}
+      arr={searchHistory}
+      setSearchHistory={setSearchHistory}
+      i={i}
+    />
+  ));
+
+  return (
+    <Wrapper onClick={() => console.log(1)}>
+      <DropdownContent>{SearchHistoryElem}</DropdownContent>
+    </Wrapper>
+  );
+});
+
 const Wrapper = styled.div`
-	position: absolute;
-	top: 70px;
-	outline: 2px solid #aacbcb;
-	border-radius: 22px;
-	width: 100%;
-	background-color: #4d4e4e;
-`
+  position: absolute;
+  top: 70px;
+  outline: 2px solid #aacbcb;
+  border-radius: 22px;
+  width: 100%;
+  background-color: #4d4e4e;
+`;
 
 const DropdownContent = styled.div`
-	position: relative;
-	font-size: 22px;
-	display: flex;
-	flex-direction: column;
-	margin: 10px 0px;
-`
+  position: relative;
+  font-size: 22px;
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0px;
+`;
 
 const Item = styled.div`
-	font-size: 18px;
-	font-family: sans-serif;
-	padding: 1px;
-	color:white;
-	font-weight:100;
-	padding-left:20px;
-	&:hover {
-		background-color: #aacbcb;
-		cursor: pointer;
-	}
-`
+  font-size: 18px;
+  font-family: sans-serif;
+  padding: 1px;
+  color: white;
+  font-weight: 100;
+  padding-left: 20px;
+  &:hover {
+    color: red;
+  }
+`;
+
+const StyledClose = styled.img`
+  width: 20px;
+  margin-right: 20px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -1,102 +1,102 @@
-import React, { useState, useRef, useEffect} from 'react';
-import styled from 'styled-components';
-import SearchIcon from '../../assets/search.svg'
-import { Dropdown } from './Dropdown';
-
-
+import React, { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+import SearchIcon from "../../assets/search.svg";
+import { Dropdown } from "./Dropdown";
 
 export const SearchBar = () => {
-	const [dropdownActive, setDropdownActive] = useState(false)
-	const wrapperRef = useRef(null)
-	const [searchActive, setSearchActive] = useState(false)
-	const [searchValue, setSearchValue] = useState('')
-	const [searchHistory, setSearchHistory] = useState([]) 
-	
+  const [dropdownActive, setDropdownActive] = useState(false);
+  const wrapperRef = useRef(null);
+  const [searchActive, setSearchActive] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchHistory, setSearchHistory] = useState([]);
 
-	const submitSearch = (e) => {
-		if (e.key === 'Enter') {
-			const value = e.target.value
-			setSearchValue(value)
-			e.target.value = ''
-			setSearchHistory(prevHistory => [...prevHistory, value])
-		}
-	}
+  const submitSearch = (e) => {
+    if (e.key === "Enter") {
+      const value = e.target.value;
+      setSearchValue(value);
+      e.target.value = "";
+      setSearchHistory([value, ...searchHistory]);
+    }
+  };
 
-	
-	const clickOutside = e => {
-		if(!(wrapperRef.current.contains(e.target))){
-			setDropdownActive(false)
-		}
-	}
+  const clickOutside = (e) => {
+    if (!wrapperRef.current.contains(e.target)) {
+      setDropdownActive(false);
+    }
+  };
 
-	useEffect(()=>{
-		document.addEventListener('click',clickOutside)
-		return () => {
-			document.removeEventListener('click',clickOutside)
-		}
-	}, [])
+  useEffect(() => {
+    document.addEventListener("click", clickOutside);
+    return () => {
+      document.removeEventListener("click", clickOutside);
+    };
+  }, []);
 
-    return (
-			<Wrapper ref={wrapperRef} onKeyDown={submitSearch}>
-				<SearchBarWrapper>
-					<StyledInput
-						width={'300px'}
-						placeholder='find users'
-						onClick={e => {
-							e.stopPropagation()
-							setDropdownActive(true)
-					
-						}}
-						onFocus={e => setSearchActive(true)}
-						onBlur={e => setSearchActive(false)}
-					/>
-					<StyledSearchLogo src={SearchIcon} isActive={searchActive}/>
-				</SearchBarWrapper>
-				{dropdownActive && <Dropdown searhHistory={searchValue} setDropdownActive={setDropdownActive} />}
-			</Wrapper>
-		)
+  return (
+    <Wrapper ref={wrapperRef} onKeyDown={submitSearch}>
+      <SearchBarWrapper>
+        <StyledInput
+          width={"300px"}
+          placeholder="find users"
+          onClick={(e) => {
+            e.stopPropagation();
+            setDropdownActive(true);
+          }}
+          onFocus={(e) => setSearchActive(true)}
+          onBlur={(e) => setSearchActive(false)}
+        />
+        <StyledSearchLogo src={SearchIcon} isActive={searchActive} />
+      </SearchBarWrapper>
+      {searchHistory.length !== 0 && dropdownActive && (
+        <Dropdown
+          searchHistory={searchHistory}
+          setSearchHistory={setSearchHistory}
+        />
+      )}
+    </Wrapper>
+  );
 };
 
 const SearchBarWrapper = styled.div`
-	display: flex;
-`
+  display: flex;
+`;
 
 const Wrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	position: relative;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
 
 const StyledInput = styled.input`
-	padding: 10px 20px;
-	font-size: 22px;
-	border-top-left-radius: 20px;
-	border-bottom-left-radius: 20px;
-	border: 1px solid black;
-	background-color: #4d4e4e;
-	&:focus {
-		outline: none;
-		color: white;
-		border: 2px solid #6da0a0;
-		border-right: none;
-	}
-	&::placeholder {
-		color: white;
-		font-family: Arial, Helvetica, sans-serif;
-		opacity: 0.5;
-	}
-	border-right: none;
-`
+  padding: 10px 20px;
+  font-size: 22px;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border: 1px solid black;
+  background-color: #4d4e4e;
+  &:focus {
+    outline: none;
+    color: white;
+    border: 2px solid #6da0a0;
+    border-right: none;
+  }
+  &::placeholder {
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+    opacity: 0.5;
+  }
+  border-right: none;
+`;
 
 const StyledSearchLogo = styled.img`
-	width: 35px;
-	padding-right: 10px;
-	border: ${props =>
-		props.isActive ? '2px solid #6da0a0' : '1px solid black'};
-	border-left: none;
-	border-top-right-radius: 20px;
-	border-bottom-right-radius: 20px;
-	cursor: pointer;
-	background-color: #4d4e4e;
-`
+  width: 35px;
+  padding-right: 10px;
+  border: ${(props) =>
+    props.isActive ? "2px solid #6da0a0" : "1px solid black"};
+  border-left: none;
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+  cursor: pointer;
+  background-color: #4d4e4e;
+`;
